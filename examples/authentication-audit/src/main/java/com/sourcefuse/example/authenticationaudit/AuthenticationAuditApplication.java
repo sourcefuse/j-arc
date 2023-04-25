cfc41abdd83e2fb7573699bf3a2bbcbfcb64b741
Harshad Kadam <harshad.kadam@sourcefuse.com>
2023-04-25 18:44:26 +0530

    feat(authentication-example): initial commit

---
+++ examples/authentication-audit/src/main/java/com/sourcefuse/example/authenticationaudit/AuthenticationAuditApplication.java
@@ -0,0 +1,17 @@
+package com.sourcefuse.example.authenticationaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan({"com.sourcefuse.jarc.services.auditservice.audit" , "com.sourcefuse.jarc.services.authservice" })
+public class AuthenticationAuditApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAuditApplication.class, args);
+	}
+
+}
---
+++ examples/authentication-audit/src/test/java/com/sourcefuse/example/authenticationaudit/AuthenticationAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcefuse.example.authenticationaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
