ce4737438add03f8eae736a8a52a313c285bebac
Harshad Kadam <harshad.kadam@sourcefuse.com>
2023-05-03 12:36:26 +0530

    feat(core): made baseEntity as base

--- libs/core/src/main/java/com/sourcefuse/jarc/core/models/base/UserModifiableEntity.java
+++ libs/core/src/main/java/com/sourcefuse/jarc/core/models/base/UserModifiableEntity.java
@@ -1,10 +1,13 @@
 
 package com.sourcefuse.jarc.core.models.base;
 
+import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.EntityListeners;
