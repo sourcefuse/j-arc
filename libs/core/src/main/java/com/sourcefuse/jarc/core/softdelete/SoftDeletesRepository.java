7cdee3395aec3428f7ce9563859abd80e7db1a6f
Harshad Kadam <harshad.kadam@sourcefuse.com>
2023-05-04 12:22:17 +0530

    feat(core): added prettier

--- libs/core/src/main/java/com/sourcefuse/jarc/core/softdelete/SoftDeletesRepository.java
+++ libs/core/src/main/java/com/sourcefuse/jarc/core/softdelete/SoftDeletesRepository.java
@@ -1,9 +1,9 @@
 package com.sourcefuse.jarc.core.softdelete;
 
+import com.sourcefuse.jarc.core.models.base.SoftDeleteEntity;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -11,41 +11,40 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.NoRepositoryBean;
 
-import com.sourcefuse.jarc.core.models.base.SoftDeleteEntity;
-
 @NoRepositoryBean
-public interface SoftDeletesRepository<T extends SoftDeleteEntity, ID extends Serializable> extends JpaRepository<T, ID> {
+public interface SoftDeletesRepository<
+  T extends SoftDeleteEntity, ID extends Serializable
+>
+  extends JpaRepository<T, ID> {
+  List<T> findAllActive();
 
-	List<T> findAllActive();
+  List<T> findAllActive(Sort sort);
 
-	List<T> findAllActive(Sort sort);
+  Page<T> findAllActive(Pageable pageable);
 
-	Page<T> findAllActive(Pageable pageable);
+  List<T> findAllActive(Iterable<ID> ids);
 
-	List<T> findAllActive(Iterable<ID> ids);
+  Optional<T> findActiveById(ID id);
 
-	Optional<T> findActiveById(ID id);
+  @Modifying
+  void softDeleteById(ID id);
 
-	@Modifying
-	void softDeleteById(ID id);
+  @Modifying
+  void softDelete(T entity);
 
-	@Modifying
-	void softDelete(T entity);
+  @Modifying
+  void softDelete(Iterable<? extends T> entities);
 
-	@Modifying
-	void softDelete(Iterable<? extends T> entities);
+  @Modifying
+  void softDeleteAll();
 
-	@Modifying
-	void softDeleteAll();
+  //	@Modifying
+  //	void scheduleSoftDelete(ID id);
+  //
+  //	@Modifying
+  //	void scheduleSoftDelete(T entity);
 
-//	@Modifying
-//	void scheduleSoftDelete(ID id);
-//
-//	@Modifying
-//	void scheduleSoftDelete(T entity);
+  long countActive();
 
-	long countActive();
-
-	boolean existsActive(ID id);
-
+  boolean existsActive(ID id);
 }
