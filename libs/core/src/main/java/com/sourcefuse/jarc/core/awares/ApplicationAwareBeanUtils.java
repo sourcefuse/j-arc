e70d60267a17fcac117304fc27df1a3e9fc645dc
Harshad Kadam <harshad.kadam@sourcefuse.com>
2023-05-10 16:21:02 +0530

    fix(core): made changes and resolved some code smells

--- libs/core/src/main/java/com/sourcefuse/jarc/core/awares/ApplicationAwareBeanUtils.java
+++ libs/core/src/main/java/com/sourcefuse/jarc/core/awares/ApplicationAwareBeanUtils.java
@@ -3,6 +3,7 @@ package com.sourcefuse.jarc.core.awares;
 import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,11 @@ public class ApplicationAwareBeanUtils {
   private ApplicationContext context;
 
   public <T> T getBean(Class<T> beanClass) {
-    return applicationContext.getBean(beanClass);
+    return context.getBean(beanClass);
   }
 
   public EntityManager getNewEntityManager() {
-    EntityManager em = applicationContext.getBean(EntityManager.class);
+    EntityManager em = context.getBean(EntityManager.class);
     return em.getEntityManagerFactory().createEntityManager();
   }
 }
