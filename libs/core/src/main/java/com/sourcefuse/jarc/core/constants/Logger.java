d6d3cee4d4becbe1bbe91726d7ae587ea6a4dbf2
Harshad Kadam <harshad.kadam@sourcefuse.com>
2023-05-23 11:35:04 +0530

    fix(core): resolved code smell

--- libs/core/src/main/java/com/sourcefuse/jarc/core/constants/Logger.java
+++ libs/core/src/main/java/com/sourcefuse/jarc/core/constants/Logger.java
@@ -3,7 +3,11 @@ package com.sourcefuse.jarc.core.constants;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class Logger {
+public final class Logger {
+
+  private Logger() {
+    throw new IllegalStateException("Utility class");
+  }
 
   public static void error(Exception e) {
     log.error("{}: {} {}", e.getMessage(), e.getCause(), e.getStackTrace());
