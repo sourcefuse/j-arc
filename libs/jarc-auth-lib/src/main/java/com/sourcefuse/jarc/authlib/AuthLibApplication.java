82ac20dc35d07a9d2d312b3fe861fa414a45182b
mlakdawala <mudassir.lakdawala@sourcefuse.com>
2023-06-22 16:49:08 +0530

    fix comment

--- libs/jarc-auth-lib/src/main/java/com/sourcefuse/jarc/authlib/AuthLibApplication.java
+++ libs/jarc-auth-lib/src/main/java/com/sourcefuse/jarc/authlib/AuthLibApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(
-  { AuthLibApplication.CORE_PACKAGE, AuthLibApplication.AUTH_LIB_PACKAGE, }
-)
+@ComponentScan({ "com.sourcefuse.jarc.core", "com.sourcefuse.jarc.authlib" })
 public class AuthLibApplication {
-  public static final String CORE_PACKAGE = "com.sourcefuse.jarc.core";
-  public static final String AUTH_LIB_PACKAGE = "com.sourcefuse.jarc.authlib";
 
   public static void main(String[] args) {
     SpringApplication.run(AuthLibApplication.class, args);
