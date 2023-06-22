a83273338af415b5717d4e89082ddb12ca4985ea
Yesha Mavani <yesha.mavani@sourcefuse.com>
2023-06-22 16:40:35 +0530

    feature toggle service

--- sandbox/feature-toggle-example/src/main/java/com/sourcefuse/jarc/sandbox/featuretoggleexample/FeatureToggleExampleApplication.java
+++ sandbox/feature-toggle-example/src/main/java/com/sourcefuse/jarc/sandbox/featuretoggleexample/FeatureToggleExampleApplication.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({FeatureToggleExampleApplication.FEATURE_EXAMPLE, FeatureToggleExampleApplication.CORE_PACKAGE,FeatureToggleExampleApplication.AUTH_PACKAGE,FeatureToggleExampleApplication.FEATURE_PACKAGE})
+@ComponentScan({"com.sourcefuse.jarc.core","com.sourcefuse.jarc.services.authservice","com.sourcefuse.jarc.services.featuretoggleservice","com.sourcefuse.jarc.sandbox.featuretoggleexample"})
 public class FeatureToggleExampleApplication {
-
-	  public static final String CORE_PACKAGE = "com.sourcefuse.jarc.core";
-	  public static final String AUTH_PACKAGE = "com.sourcefuse.jarc.services.authservice";
-	  public static final String FEATURE_PACKAGE = "com.sourcefuse.jarc.services.featuretoggleservice";
-	  public static final String FEATURE_EXAMPLE = "com.sourcefuse.jarc.sandbox.featuretoggleexample";
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeatureToggleExampleApplication.class, args);
--- services/feature-toggle-service/src/main/java/com/sourcefuse/jarc/services/featuretoggleservice/FeatureToggleServiceApplication.java
+++ services/feature-toggle-service/src/main/java/com/sourcefuse/jarc/services/featuretoggleservice/FeatureToggleServiceApplication.java
@@ -8,28 +8,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.sourcefuse.jarc.core.repositories.SoftDeletesRepositoryImpl;
 
 @SpringBootApplication()
-// @EnableAutoConfiguration
 @ComponentScan({ "com.sourcefuse.jarc.services.featuretoggleservice",
 		"com.sourcefuse.jarc.core", "com.sourcefuse.jarc.services.authservice" })
-// @EnableJpaRepositories
-// @EnableJpaRepositories(basePackages = {
-// "com.sourcefuse.jarc.services.featuretoggleservice.repositories" })
-// @EnableJpaRepositories(basePackages = {
-// "com.sourcefuse.jarc.core.repositories" })
 @EnableJpaRepositories(repositoryBaseClass = SoftDeletesRepositoryImpl.class)
 
 public class FeatureToggleServiceApplication {
-
-	/**
-	 * TODO: need to remove this code when authentication service is integrated This
-	 * code is added to allow access to all url in this app for temporary purpose
-	 */
-	// @Bean
-	// public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	// return http.csrf(CsrfConfigurer::disable)
-	// .authorizeHttpRequests(requests ->
-	// requests.requestMatchers("/**").permitAll()).build();
-	// }
 
 	public static void main(String[] args) {
 
