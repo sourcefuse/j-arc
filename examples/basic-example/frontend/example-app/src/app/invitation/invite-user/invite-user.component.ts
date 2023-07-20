0543c054ec446b509aae6146e2156b1f3060b972
Harshad Kadam <harshad.kadam@sourcefuse.com>
2023-07-20 11:25:25 +0530

    added user, audit components

--- examples/basic-example/frontend/example-app/src/app/invitation/invite-user/invite-user.component.ts
+++ examples/basic-example/frontend/example-app/src/app/invitation/invite-user/invite-user.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class InviteUserComponent implements OnInit {
 
   userDetails:any ={
-    authProvider:"internal"
+    authProvider:"keycloak"
   }
 
   constructor() { }
