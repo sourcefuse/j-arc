a11aa6a63449b76618ee223ef92a1885b48acc1e
ADILSDE <adil.shaikh@sourcefuse.com>
2023-08-03 17:31:49 +0530

    sonar fixed 3..

--- services/user-tenant-service/src/main/java/com/sourcefuse/jarc/services/usertenantservice/dto/UserView.java
+++ services/user-tenant-service/src/main/java/com/sourcefuse/jarc/services/usertenantservice/dto/UserView.java
@@ -14,13 +14,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
-import java.util.UUID;
 
 @Entity
 @Data
