64cc4d0b0f7d34cd44145ff19e530a237608373f
harshadk-sourcefuse <115210414+harshadk-sourcefuse@users.noreply.github.com>
2023-08-05 15:17:21 +0530

    Feature to enable CORS  (#50)
    cors-feature

---
+++ libs/jarc-auth-lib/src/main/java/com/sourcefuse/jarc/authlib/cors/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.sourcefuse.jarc.authlib.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Value("${cors.url:#{null}}")
+  String corsUrl;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    if (corsUrl != null) {
+      registry
+        .addMapping("/**")
+        .allowedOrigins(corsUrl)
+        .allowedMethods("*")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+    }
+  }
+}
